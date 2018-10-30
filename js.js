ed29880ca6cfc4576e76196955674267e81e5660
nickweyermann <31235664+nickweyermann@users.noreply.github.com>
2018-10-30 16:11:05 +0100

    refreshed everything

--- js.js
+++ js.js
@@ -1,18 +1,22 @@
-$(document).ready(function () {
-    "use strict";
-	var image1 = 0;
-	var image2 = 0;
-    $(".btnStats").click(function () {
-        $('html,body').animate({
-            scrollTop: $(document).height() - $(window).height()
-        }, 'slow');
-    });
-	$("#image1").click(function () {
-		image1++;
-		alert("Bild1 angeclickt: " + image1);
-	});
-	$("#image2").click(function () {
-		image2++;
-		alert("Bild2 angeclickt: " + image2);
-	});
+var image1 = 0;
+var image2 = 0;
+
+$("#showStats").click(function () {
+    var site = window.location.href;
+    site += "statistik.html";
+    window.location.href = site;
+});
+
+$("#showMain").click(function () {
+    var site = window.location.origin;
+    site += "/CopOrDrop/";
+    window.location.href = site;
+});
+$("#image1").click(function () {
+    image1++;
+    alert("Bild1 angeclickt: " + image1);
+});
+$("#image2").click(function () {
+    image2++;
+    alert("Bild2 angeclickt: " + image2);
 });
