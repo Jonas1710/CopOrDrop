2607d5cbf1ea0d17596a3c353f0f8be7de46d155
nickweyermann <31235664+nickweyermann@users.noreply.github.com>
2018-11-16 15:57:58 +0100

    registration & login -> added error msg

--- js/login.js
+++ js/login.js
@@ -1,13 +1,28 @@
 var regex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z.,:;\-+_!?@#*=/\\()\[\]{}]{8,}$/;
+
 $("#btn_login").click(function () {
-    if ($("#input_username").val() != "") {
-        $("#lbl_username").css("color", "");
-    } else {
+    var msg = "";
+    if ($("#input_username").val() == "") {
         $("#lbl_username").css("color", "red");
-    }
-    if ($("#input_password").val().match(regex)) {
-        $("#lbl_password").css("color", "");
+        msg += "<p>Benutzername ist leer</p>";
     } else {
+        $("#lbl_username").css("color", "");
+    }
+    if ($("#input_password").val() == "") {
         $("#lbl_password").css("color", "red");
+        msg += "<p>Passwort ist leer</p>";
+    } else if (!$("#input_password").val().match(regex)) {
+        $("#lbl_password").css("color", "red");
+        msg += "<p>Passwort ist falsch</p>";
+    } else {
+        $("#lbl_password").css("color", "");
+    }
+    if (msg == "") {
+        //Login
+        
+        $("#error").hide();
+    } else {
+        $("#error").show();
+        $("#error").html(msg);
     }
 });
--- js/registration.js
+++ js/registration.js
@@ -1,18 +1,41 @@
 var regex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z.,:;\-+_!?@#*=/\\()\[\]{}]{8,}$/;
+
 $("#btn_register").click(function () {
-    if ($("#input_username").val() != "") {
-        $("#lbl_username").css("color", "");
-    } else {
+    var msg = "";
+    if ($("#input_username").val() == "") {
         $("#lbl_username").css("color", "red");
-    }
-    if ($("#input_password").val().match(regex)) {
-        $("#lbl_password").css("color", "");
+        msg += "<p>Benutzername ist leer</p>";
     } else {
+        $("#lbl_username").css("color", "");
+    }
+    if ($("#input_password").val() == "") {
         $("#lbl_password").css("color", "red");
-    }
-    if ($("#input_password_2").val() == $("#input_password").val()) {
-        $("#lbl_password_2").css("color", "");
+        msg += "<p>Passwort ist leer</p>";
+    } else if (!$("#input_password").val().match(regex)) {
+        $("#lbl_password").css("color", "red");
+        msg += "<p>Passwort entspricht nicht den Richtlinien</p>";
+        msg += "<p> - Min. 1 Grossbuchstabe</p>";
+        msg += "<p> - Min. 1 Kleinbuchstabe</p>";
+        msg += "<p> - Min. 1 Zahl</p>";
+        msg += "<p> - Min. 8 Zeichen</p>";
     } else {
+        $("#lbl_password").css("color", "");
+    }
+    if ($("#input_password_2").val() == "") {
         $("#lbl_password_2").css("color", "red");
+        msg += "<p>Wiederholtes Passwort ist leer</p>";
+    } else if ($("#input_password_2").val() != $("#input_password").val()) {
+        $("#lbl_password_2").css("color", "red");
+        msg += "<p>Wiederholtes Passwort stimmt nicht überein</p>";
+    } else {
+        $("#lbl_password_2").css("color", "");
+    }
+    if (msg == "") {
+        //Registrieren
+        
+        $("#error").hide();
+    } else {
+        $("#error").show();
+        $("#error").html(msg);
     }
 });
